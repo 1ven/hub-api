e27a6b106dbe0aedb6ab738018e13bda0cefa70a
1ven <1ven.mironchik@gmail.com>
2017-12-02 16:33:47 +0300

    Implement github auth

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import http from "http";
 import { compose } from "ramda";
 import { adapter } from "litera";
 import router from "litera-router";
-import bodyParser from "litera-body-parser";
 import errorHandler from "litera-error-handler";
 import cors from "litera-cors";
 import routes from "./routes";
 
-const app = compose(errorHandler(), cors(), bodyParser())(router(routes));
+const app = compose(errorHandler(), cors())(router(routes));
 
 http.createServer(adapter(app)).listen(8080, () => {
   console.log("Listening at 8080");
--- src/routes/auth/github/index.js
+++ src/routes/auth/github/index.js
@@ -1,34 +1,51 @@
-import url from "url";
-import queryString from "querystring";
+import { merge, withStatus } from "litera";
+import { method, fork, match } from "litera-router";
+import { readQuery } from "litera-querystring";
+import { readCookie, withCookie } from "litera-cookie";
+import { withRedirect } from "litera-redirect";
+import join from "url-join";
 import axios from "axios";
-import { method, fork } from "litera-router";
+
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
 
 export default fork(
-  method("POST", () => async req => {
-    const { code } = req.body;
-    const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
+  method(
+    "GET",
+    fork(
+      match("/", () => async req => {
+        const { redirect_uri } = readQuery(req);
 
-    const response = await axios({
-      url: "https://github.com/login/oauth/access_token",
-      method: "POST",
-      headers: {
-        accept: "application/json"
-      },
-      data: {
-        code,
-        client_id: GITHUB_CLIENT_ID,
-        client_secret: GITHUB_CLIENT_SECRET
-      }
-    });
+        return merge(
+          withRedirect(
+            `https://github.com/login/oauth/authorize?scope=user:email&client_id=${
+              GITHUB_CLIENT_ID
+            }`
+          ),
+          !!redirect_uri &&
+            withCookie("redirect_uri", redirect_uri, { maxAge: 60 })
+        );
+      }),
+      match("/callback", () => async req => {
+        const { code } = readQuery(req);
+        const { redirect_uri } = readCookie(req);
 
-    return {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        access_token: response.data.access_token
+        const response = await axios({
+          url: "https://github.com/login/oauth/access_token",
+          method: "POST",
+          headers: {
+            accept: "application/json"
+          },
+          data: {
+            code,
+            client_id: GITHUB_CLIENT_ID,
+            client_secret: GITHUB_CLIENT_SECRET
+          }
+        });
+
+        return merge(
+          withRedirect(join(redirect_uri, response.data.access_token))
+        );
       })
-    };
-  })
+    )
+  )
 );
