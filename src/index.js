96c0c7262d99036fcaed40ea7ab8bd0f538f45cf
1ven <1ven.mironchik@gmail.com>
2017-12-04 21:57:30 +0300

    Provide db instance

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { adapter } from "litera";
 import router from "litera-router";
 import errorHandler from "litera-error-handler";
 import cors from "litera-cors";
+import { db } from "core/database";
 import app from "./application";
 
 const atom = compose(errorHandler(), cors(), router)(app);
 
-http.createServer(adapter(atom)).listen(8080, () => {
+http.createServer(adapter(atom, { db })).listen(8080, () => {
   console.log("Listening at 8080");
 });
