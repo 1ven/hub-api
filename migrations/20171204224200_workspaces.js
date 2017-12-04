7cf5e8aae96df21e0474dd7f17352e6bb20bb585
1ven <1ven.mironchik@gmail.com>
2017-12-04 23:24:31 +0300

    Add workspaces migration

---
+++ migrations/20171204224200_workspaces.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("workspaces", function(table) {
+    table.increments("id").primary();
+    table.string("slug").notNullable();
+    table.integer("assigned_to").notNullable();
+    table.unique(["assigned_to", "slug"]);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("workspaces");
+};
