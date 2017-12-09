25717ecfab5ff908a76b74e5cec13aa08a02fb82
1ven <1ven.mironchik@gmail.com>
2017-12-09 11:35:09 +0300

    Change migrations

--- migrations/20171209112859_workspaces.js
+++ migrations/20171209112859_workspaces.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("workspaces", function(table) {
     table.increments("id").primary();
     table.string("slug").notNullable();
-    table.integer("assigned_to").notNullable();
+    table.string("assigned_to").notNullable();
     table.unique(["assigned_to", "slug"]);
   });
 };
